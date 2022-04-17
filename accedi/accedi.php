a38685ba86f3fcecc883f3d611d4dff6f8da6c89
ponseh <lucaponseg@gmail.com>
2022-04-17 16:14:38 +0200

    fixed login, other improvements

--- accedi/accedi.php
+++ accedi/accedi.php
@@ -1,15 +1,23 @@
 <?php
+    session_start();
+
     require_once('..\database\database.php');
 
-    $stmt = $conn->prepare('SELECT * FROM tUtenti WHERE Nickname=?');
-    $stmt->bind_param("s", $_POST['Utente']);
+    if(isset($_POST['InviaEmail'])) {
+        $stmt = $conn->prepare('SELECT * FROM tUtenti WHERE Email=?');
+        $stmt->bind_param("s", $_POST['Email']);
+
+    } else if(isset($_POST['InviaNick'])) {
+        $stmt = $conn->prepare('SELECT * FROM tUtenti WHERE Nickname=?');
+        $stmt->bind_param("s", $_POST['Nickname']);
+    }
+
     $stmt->execute();
     $result = $stmt->get_result();
 
     $arrayRes = $result->fetch_assoc();
 
-    //if(password_verify($_POST['Password'], $arrayRes['Password'])) {
-        if()
-    //}
-    print_r($arrayRes);
+    if(password_verify($_POST['Password'], $arrayRes['Password'])) {
+        $_SESSION['Utente_Loggato'] = $arrayRes;
+    }
 ?>
